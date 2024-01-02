1239c5aa956aada96c994def3eafd0d9a19d395c
AayushKK <104511889+AayushKK@users.noreply.github.com>
2024-01-02 21:54:42 +0545

    Update app.js

--- DOM event bubble/app.js
+++ DOM event bubble/app.js
@@ -2,48 +2,68 @@
 let screenbtm=document.getElementById("screen-bottom");
 let circle=document.getElementsByClassName("circle");
 let nav=document.getElementsByClassName("box");
+
 // initialize variables
 let counter=0;
 let bubble='';
+
+//set the timer
+nav[0].innerHTML=7;
+let timer=7;
+
 // create 200 random bubbles
-for(let i=0;i<200;i++){
+function createBubbles(){  
+    bubble='';
+    for(let i=0;i<200;i++){
+    
     let num= Math.floor(Math.random()*100);
     bubble += `<div class='circle'>${num}</div>`;
 
+    }
+    // display bubbles on screen
+    screenbtm.innerHTML=bubble;
 }
-// display bubbles on screen
-screenbtm.innerHTML=bubble;
 
-
-// add event listener to each bubble and update score
-for(let i=0;i<circle.length;i++){
-    circle[i].addEventListener("click",function(){
-        // change color of bubble
-        circle[i].style.backgroundColor="red";
+// add event listener to bubbles
+document.querySelector("#screen-bottom").addEventListener("click",function(details){
+    
+    // check if the target is a bubble
+    if(details.target.className=="circle"){
         // count score
-        counter = counter + parseInt(circle[i].innerHTML);
-                // update score
-                nav[1].innerHTML=counter;
-    })
-}
-//set the timer
-nav[0].innerHTML=20;
-let timer=20;
-// update timer
-let time=setInterval(function(){
-   if(timer>0){
-       timer--;
-       nav[0].innerHTML=timer;
-   }
-   else{
-        // stop timer
-       clearInterval(time);
-       // display final score
-       screenbtm.innerHTML='<h1 style="color:blue">Game Over!!!<br>Your Final Score is:' + nav[1].innerHTML + '</h1>';
+        counter+=parseInt(details.target.innerHTML);
+        // update score
+        nav[1].innerHTML=counter;
+        // reload bubbles
+        createBubbles();
        
-   }
-}, 1000);
+    }
+});
 
+
+function gameOver(){
+// display final score
+screenbtm.innerHTML=`<h1 style="color:blue">Game Over!!!<br>Your Final Score is: ${nav[1].innerHTML} </h1>`;
+}
+
+// update the timer
+function updateTime(){
+        let time= setInterval(function(){
+            if(timer>0){
+                timer--;
+                nav[0].innerHTML=timer;
+            }
+            else{
+                 // stop timer
+                clearInterval(time);
+                //game over
+                gameOver();
+                
+            }
+         }, 1000);
+    }
+  
+createBubbles();
+updateTime();
 
 
 
